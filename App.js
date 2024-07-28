d20c0e7ebd78498137494c67b1cfbe869cc906e6
Daniel Rodríguez <danielcarlosrodriguez@gmail.com>
2024-07-28 17:31:28 -0300

    Fix image import and styling in App.js

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from "react-native";
+import icon from "./assets/icon.png"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={icon} style={{ width: 100, height: 100 }} />
+      <Text style={{color: "#fff"}}>
+        Open up App.js to start working on your app!z Lorem ipsum dolor sit,
+        amet consectetur adipisicing elit. Magnam, tenetur excepturi corporis
+        assumenda cumque, aspernatur quas labore perspiciatis error vel, animi
+        laudantium aperiam harum nobis? Eius, quod quae. Voluptatum, minus!
+      </Text>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +20,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
 });
